7ffe345ccec49c2b3ad24b086a4082fc42df6ec2
Donald Pakkies <donaldpakkies@gmail.com>
2018-05-27 21:29:50 +0200

    Feat: updated the "Authenticate" and "Guest" middleware's
    Added the request class

--- Middleware/Authenticate.php
+++ Middleware/Authenticate.php
@@ -3,10 +3,11 @@
 namespace ModulusPHP\Http\Middleware;
 
 use App\Core\Auth;
+use ModulusPHP\Http\Requests\Request;
 
 class Authenticate
 {
-  public function handle()
+  public function handle(Request $request)
   {
     if (Auth::isGuest() == true) {
       return true;
