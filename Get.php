2caf12da16e9e6d266724b8912413c397e011da7
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 12:34:39 +0200

    Feat: has
    Get request get variables

---
+++ Get.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modulus\Http;
+
+class Get
+{
+  /**
+   * Check if query contains key
+   *
+   * @param mixed $key
+   * @return bool
+   */
+  public static function has($key) : bool
+  {
+    if (isset($_GET[$key])) return true;
+    return false;
+  }
+
+  /**
+   * Delete query key
+   *
+   * @param mixed $key
+   * @return void
+   */
+  public static function delete($key)
+  {
+    unset($_GET[$key]);
+  }
+
+  /**
+   * Return query key
+   *
+   * @param mixed $key
+   * @return mixed
+   */
+  public static function key($key)
+  {
+    return $_GET[$key];
+  }
+}
