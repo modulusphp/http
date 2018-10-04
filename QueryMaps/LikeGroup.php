2b1d1f077664b5a7fcdba843e944ea8abaf34ab7
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 12:33:23 +0200

    Feat: added 2 query maps
    Model and Groupable (collections) query maps

---
+++ QueryMaps/LikeGroup.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modulus\Http\QueryMaps;
+
+use Illuminate\Database\Eloquent\Collection;
+use Modulus\Utility\{Groupable, RouteQuery};
+
+class LikeGroup extends RouteQuery
+{
+  /**
+   * Handle Query Map
+   *
+   * @param Groupable $group
+   * @param mixed $field
+   * @param mixed $value
+   * @return Collection
+   */
+  protected function handle(Groupable $group, $field, $value, $name) : Groupable
+  {
+    return $group->assign($group->model()::where($field, 'like', '%' . trim($value) . '%')->get() ?? new Collection);
+  }
+}
---
+++ QueryMaps/LikeModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modulus\Http\QueryMaps;
+
+use Modulus\Utility\RouteQuery;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class LikeModel extends RouteQuery
+{
+  /**
+   * Handle Query Map
+   *
+   * @param EloquentModel $model
+   * @param mixed $field
+   * @param mixed $value
+   * @return EloquentModel
+   */
+  protected function handle(EloquentModel $model, $field, $value) : EloquentModel
+  {
+    return $model::where($field, 'like', '%' . trim($value) . '%')->first() ?? $model;
+  }
+}
