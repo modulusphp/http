943ab7a348661d33e32ba741e7857180b3861fac
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 12:33:54 +0200

    Feat: generates a new csrf token and assigns the timestamp

---
+++ CSRF.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modulus\Http;
+
+use Modulus\Http\Request;
+use Modulus\Utility\Events;
+
+class CSRF
+{
+  /**
+   * Generate csrf token
+   *
+   * @return string
+   */
+  public static function generate()
+  {
+    $_SESSION['_cksal']  = self::genTimeStamp();
+    $_SESSION['_saini'] = bin2hex(random_bytes(30));
+
+    return $_SESSION['_saini'];
+  }
+
+  /**
+   * Generate timestamp
+   *
+   * @return string
+   */
+  public static function genTimeStamp()
+  {
+    return base64_encode(date('Y-m-d H:i:s'));
+  }
+}
