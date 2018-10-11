005dbf4eafa2b7b4ee10d4e56fb75f833ad068fc
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-11 23:52:05 +0200

    Feat: added a base http foundation class

---
+++ Kernel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modulus\Http;
+
+class Kernel
+{
+  /**
+   * $routeModelBinding
+   *
+   * @var array
+   */
+  public static $routeModelBinding = [];
+
+  /**
+   * $middleware
+   *
+   * @var array
+   */
+  public static $middleware = [];
+
+  /**
+   * $middlewareGroup
+   *
+   * @var array
+   */
+  public static $middlewareGroup = [];
+
+  /**
+   * $routeMiddleware
+   *
+   * @var array
+   */
+  public static $routeMiddleware = [];
+}
