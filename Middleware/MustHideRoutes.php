6d9dc3761bb85d312dc8128247416e7126442775
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 16:02:37 +0200

    Feat: makes routes private

---
+++ Middleware/MustHideRoutes.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modulus\Http\Middleware;
+
+use Modulus\Http\Status;
+use Modulus\Utility\Events;
+use Modulus\Framework\Exceptions\NotFoundHttpException;
+
+class MustHideRoutes
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Modulus\Http\Request $request
+   * @param  bool $continue
+   * @return bool $continue
+   */
+  public function handle($request, $continue) : bool
+  {
+    $http = ($request->server->has('HTTPS') && $request->server('HTTPS') != 'off') ? 'https://' : 'http://';
+
+    if (
+      $request->server->has('HTTP_REFERER') &&
+      starts_with($request->server->http_referer, $http . $request->headers->host)
+      ) {
+      return $continue;
+    }
+
+    throw new NotFoundHttpException($request->isAjax(), 404);
+  }
+}
