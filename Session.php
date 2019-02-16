0e893cddf3b89d3663ffeb20bfcdc1f33e1923d1
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-16 02:18:20 +0200

    Feat: Check if session already has variables. If variables have already been set, merge the new variables with the old ones.

--- Session.php
+++ Session.php
@@ -51,6 +51,15 @@ class Session
   */
   public static function flash(string $key, $value = null)
   {
-    return $_SESSION['application']['with'] = [$key => $value];
+    /**
+     * Check if session already has variables.
+     *
+     * If variables have already been set, merge the new
+     * variables with the old ones.
+     */
+    return $_SESSION['application']['with'] = array_merge(
+                                                isset($_SESSION['application']['with']) ? $_SESSION['application']['with'] : [],
+                                                [$key => $value]
+                                              );
   }
 }
